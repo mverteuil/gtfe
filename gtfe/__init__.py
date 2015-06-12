72085550b3fdbe4292cc7bd68ec113365c670dd9
M. de Verteuil <onceuponajooks@gmail.com>
2015-06-12 14:20:10 -0400

    Reduced to one function

--- gtfe/__init__.py
+++ gtfe/__init__.py
@@ -1,26 +1,23 @@
 from email import utils as emailutils
-import time
+import logging
 
 from dateutil import tz
 
+
+log = logging.getLogger(__name__)
 
 RFC2822_FORMAT = '%a, %d %b %Y %T %z'
 
 
 def get_epoch_for_datetime(datetime):
-    """ Given a timezone-naive datetime-like object, calculates the number of seconds that have elapsed since the Unix epoch.
+    """ Given a datetime-like object, calculates the number of seconds that have elapsed since the Unix epoch.
 
-    Assumes that the datetime is in local time.
+    If given a timezone-naive object, it is assumed to be local time.
 
     """
-    timezone_aware_datetime = datetime.replace(tzinfo=tz.tzlocal())
-    return get_epoch_for_datetime_tz(timezone_aware_datetime)
-
-
-def get_epoch_for_datetime_tz(datetime):
-    """ Given a timezone aware datetime-like object, calculates the number of seconds that have elapsed since the Unix epoch. """
-    if not all((datetime, datetime.tzinfo)):
-        raise ValueError("Expected a timezone-aware datetime, received {}.".format(datetime))
+    if not (datetime.tzinfo):
+        log.warning("Received naive datetime-like object. Assuming local time.")
+        datetime = datetime.replace(tzinfo=tz.tzlocal())
     rfc2822_formatted_datetime = datetime.strftime(RFC2822_FORMAT)
     time_tuple = emailutils.parsedate_tz(rfc2822_formatted_datetime)
     return emailutils.mktime_tz(time_tuple)
--- test_datetimes.py
+++ test_datetimes.py
@@ -2,12 +2,12 @@ from datetime import datetime
 import time
 
 from dateutil import tz
-import pytest
 
 import gtfe
 
 
 def almost_equal(left, right, threshold=10):
+    """ Determines if the values given are within the threshold distance of each other. """
     return abs(left - right) < threshold
 
 
@@ -20,37 +20,29 @@ class TestDatetimes(object):
         self.tz_aware_utc_epoch_start = datetime(1970, 1, 1, 0, 0, 0, 0, tzinfo=tz.tzutc())
         self.tz_aware_local_epoch_start = self.tz_aware_utc_epoch_start.astimezone(tz.tzlocal())
         self.tz_naive_local_epoch_start = self.tz_aware_local_epoch_start.replace(tzinfo=None)
+        self.expected_epoch_now = round(time.time())
+        self.expected_epoch_start = 0
 
-
-    def test_tz_aware_now(self):
+    def test_aware_now(self):
         """ Should respect timezone awareness, producing the seconds since the epoch. """
-        expected_epoch_now = round(time.time())
-        actual_epoch_now = gtfe.get_epoch_for_datetime_tz(self.tz_aware_now)
-        valid = almost_equal(expected_epoch_now, actual_epoch_now)
-        assert valid, (expected_epoch_now, actual_epoch_now)
+        actual_epoch_now = gtfe.get_epoch_for_datetime(self.tz_aware_now)
+        valid = almost_equal(self.expected_epoch_now, actual_epoch_now)
+        assert valid, (self.expected_epoch_now, actual_epoch_now)
 
-    def test_tz_naive_now(self):
+    def test_naive_now(self):
         """ Should respect timezone naiveness, producing the seconds since the epoch. """
-        expected_epoch_now = round(time.time())
         actual_epoch_now = gtfe.get_epoch_for_datetime(self.tz_naive_now)
-        valid = almost_equal(expected_epoch_now, actual_epoch_now)
-        assert valid, (expected_epoch_now, actual_epoch_now)
+        valid = almost_equal(self.expected_epoch_now, actual_epoch_now)
+        assert valid, (self.expected_epoch_now, actual_epoch_now)
 
-    def test_tz_aware_utc_epoch_start(self):
+    def test_aware_utc_epoch_start(self):
         """ Should respect timezone awareness, producing the seconds since the epoch. """
-        expected_epoch_start = 0
-        actual_epoch_start = gtfe.get_epoch_for_datetime_tz(self.tz_aware_utc_epoch_start)
-        valid = almost_equal(expected_epoch_start, actual_epoch_start)
-        assert valid, (expected_epoch_start, actual_epoch_start)
+        actual_epoch_start = gtfe.get_epoch_for_datetime(self.tz_aware_utc_epoch_start)
+        valid = almost_equal(self.expected_epoch_start, actual_epoch_start)
+        assert valid, (self.expected_epoch_start, actual_epoch_start)
 
-    def test_tz_naive_epoch_start(self):
+    def test_naive_epoch_start(self):
         """ Should respect timezone naiveness, producing the seconds since the epoch. """
-        expected_epoch_start = 0
         actual_epoch_start = gtfe.get_epoch_for_datetime(self.tz_naive_local_epoch_start)
-        valid = almost_equal(expected_epoch_start, actual_epoch_start)
-        assert valid, (expected_epoch_start, actual_epoch_start)
-
-    def test_tz_naive_with_tz_aware_dispatch(self):
-        """ Should raise an assertion error if using naive datetimes with tz aware version. """
-        with pytest.raises(ValueError):
-            gtfe.get_epoch_for_datetime_tz(self.tz_naive_now)
+        valid = almost_equal(self.expected_epoch_start, actual_epoch_start)
+        assert valid, (self.expected_epoch_start, actual_epoch_start)
